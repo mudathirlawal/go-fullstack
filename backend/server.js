858ee2c9a5eea85109883817025723aa9be03a97
Mudathir Lawal <nigercode@gmail.com>
2020-02-25 02:15:57 +0100

    creates node server using imported express app

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 
 const app = require( './app' );
 const http = require( 'http' );
-const server = http.createServer(  );
+const server = http.createServer( app );
 server.listen( process.env.PORT || 3000 );
