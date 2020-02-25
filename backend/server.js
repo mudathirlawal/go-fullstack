524d975500d40057e50ef7d44b9faa540cf668e5
Mudathir Lawal <nigercode@gmail.com>
2020-02-25 02:12:48 +0100

    removes function argument of the createServer() method

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 
 const app = require( './app' );
 const http = require( 'http' );
-const server = http.createServer( ( request, response ) => {
-    response.end( '\n\tThis is my server response.\n' );
-} );
+const server = http.createServer(  );
 server.listen( process.env.PORT || 3000 );
