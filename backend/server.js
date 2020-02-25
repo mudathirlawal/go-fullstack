1b6a8c0bb8fdf26eec9c83744fb11fa26df20c58
Mudathir Lawal <nigercode@gmail.com>
2020-02-25 02:41:38 +0100

    sets listening port for app - app now knows where to listen

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@
 const app = require( './app' );
 const http = require( 'http' );
 
-
+app.set( 'port', process.env.PORT || 3000 );
 const server = http.createServer( app );
 server.listen( process.env.PORT || 3000 );
