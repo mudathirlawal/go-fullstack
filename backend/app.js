1c6e1cbad5c79ae7781572fe6dfdc7ba7c8bc00d
Mudathir Lawal <nigercode@gmail.com>
2020-02-25 03:11:57 +0100

    adds a test endpoint to app

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require( 'express' );
 const app = express();
 
+app.use( ( request, response ) => {
+    response.json( { message: 'Your request was successful!' } );
+} );
 module.exports = app;
