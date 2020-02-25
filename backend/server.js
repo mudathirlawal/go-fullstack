e53a4d575d7064265c815e69eb5e2b3dc765962d
Mudathir Lawal <nigercode@gmail.com>
2020-02-25 02:07:00 +0100

    imports app in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 
+const app = require( './app' );
 const http = require( 'http' );
 const server = http.createServer( ( request, response ) => {
     response.end( '\n\tThis is my server response.\n' );
