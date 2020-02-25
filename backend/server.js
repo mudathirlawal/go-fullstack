34b998357d5c85d468d48319030884fec5820648
Mudathir Lawal <nigercode@gmail.com>
2020-02-25 01:33:17 +0100

    creates listening port for node server

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,4 @@ const http = require( 'http' );
 const server = http.createServer( ( request, response ) => {
     response.end( '\n\tThis is my server response.\n' );
 } );
+server.listen( process.env.PORT || 3000 );
